f55d170ab933ce96a0280bb4f8915f95e010aecf
alirezafakhar <avatar_ali95@yahoo.com>
2022-07-15 17:35:46 +0430

    validate irancel va raitel

--- src/validate.js
+++ src/validate.js
@@ -1,35 +1,33 @@
-const phoneInput = document.getElementById("email_phone");
+const phoneInput = document.getElementById('email_phone');
 
-phoneInput.addEventListener("input", () => {
-  phoneInput.setCustomValidity("");
+phoneInput.addEventListener('input', () => {
+  phoneInput.setCustomValidity('');
   phoneInput.checkValidity();
 });
 
-phoneInput.addEventListener("invalid", () => {
-  if (phoneInput.value === "") {
-    phoneInput.setCustomValidity("شماره وارد کنید");
+phoneInput.addEventListener('invalid', () => {
+  if (phoneInput.value === '') {
+    phoneInput.setCustomValidity('شماره وارد کنید');
   } else {
-    phoneInput.setCustomValidity(
-      "باید شماره تلفن به این شکل باشد:09151234567"
-    );
+    phoneInput.setCustomValidity('باید شماره تلفن به این شکل باشد:09151234567');
   }
 });
 
 function validatePhoneNumber(input_str) {
-  const phoneRegex = /^09(1[0-9])-?[0-9]{7}$/;
+  const phoneRegex = /^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}$/;
 
   return phoneRegex.test(input_str);
 }
 
 function validateForm(event) {
-  const phone = document.getElementById("email_phone").value;
+  const phone = document.getElementById('email_phone').value;
   if (!validatePhoneNumber(phone)) {
-    document.getElementById("phone_error").classList.remove("hidden");
+    document.getElementById('phone_error').classList.remove('hidden');
   } else {
-    document.getElementById("phone_error").classList.add("hidden");
-    alert("validation success");
+    document.getElementById('phone_error').classList.add('hidden');
+    alert('validation success');
   }
   event.preventDefault();
 }
 
-document.getElementById("myform").addEventListener("submit", validateForm);
+document.getElementById('myform').addEventListener('submit', validateForm);
