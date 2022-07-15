8aa65140d0d510f7e7ab93c6c2860fc8f1b24b13
Mohammad Honarvar <honarvar.info@gmail.com>
2022-07-15 16:07:18 +0330

    fix: move `validate.js` to `src` folder & fix address of `validate.js`

---
+++ src/validate.js
@@ -0,0 +1,35 @@
+const phoneInput = document.getElementById("email_phone");
+
+phoneInput.addEventListener("input", () => {
+  phoneInput.setCustomValidity("");
+  phoneInput.checkValidity();
+});
+
+phoneInput.addEventListener("invalid", () => {
+  if (phoneInput.value === "") {
+    phoneInput.setCustomValidity("شماره وارد کنید");
+  } else {
+    phoneInput.setCustomValidity(
+      "باید شماره تلفن به این شکل باشد:09151234567"
+    );
+  }
+});
+
+function validatePhoneNumber(input_str) {
+  const phoneRegex = /^09(1[0-9])-?[0-9]{7}$/;
+
+  return phoneRegex.test(input_str);
+}
+
+function validateForm(event) {
+  const phone = document.getElementById("email_phone").value;
+  if (!validatePhoneNumber(phone)) {
+    document.getElementById("phone_error").classList.remove("hidden");
+  } else {
+    document.getElementById("phone_error").classList.add("hidden");
+    alert("validation success");
+  }
+  event.preventDefault();
+}
+
+document.getElementById("myform").addEventListener("submit", validateForm);
